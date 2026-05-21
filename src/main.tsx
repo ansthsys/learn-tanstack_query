cf59f06304146d3211713c5e5216a88dbb0df512
azmiy.thufail <azmiy.thufail@aqi.co.id>
2026-05-21 17:09:29 +0700

    feat: replace legacy styles with tailwind v4 and shadcn/ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@/assets/styles/index.css";
+import "@/assets/styles/app.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import { resolve } from 'path'
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
-      '@': resolve(import.meta.dirname, 'src')
-    }
-  }
-})
+      "@": resolve(import.meta.dirname, "src"),
+    },
+  },
+});
